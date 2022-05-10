d599b74951b144a2fb67c1d0563da6462adf0d9d
Zheng <aheze@getfind.app>
2022-05-10 16:39:51 -0700

    Initial Commit

---
+++ RealityKitLaunchScreen/RealityKitLaunchScreenApp.swift
@@ -0,0 +1,19 @@
+//
+//  RealityKitLaunchScreenApp.swift
+//  RealityKitLaunchScreen
+//
+//  Created by A. Zheng (github.com/aheze) on 5/10/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct RealityKitLaunchScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
